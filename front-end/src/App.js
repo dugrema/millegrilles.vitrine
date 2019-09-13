b607241dd93f8fab2b0c4a6bf2179a1e6679a80e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-09-13 09:28:08 -0400

    Ajout w3.css, menu pour mobile.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
+import './w3.css';  // Copie de https://www.w3schools.com/w3css/4/w3.css
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 import {SenseursPassifsVitrine} from './domaines/SenseursPassifs';
 
@@ -26,6 +28,31 @@ class App extends React.Component {
   componentDidMount() {
   }
 
+  header() {
+    return (
+      <header className="w3-container w3-red w3-center">
+        <h1 className="w3-margin w3-jumbo">Vitrine</h1>
+        <p className="w3-xlarge">MilleGrille XXXX</p>
+      </header>
+    );
+  }
+
+  contenuPageAccueil() {
+    return (
+      <div class="w3-row-padding w3-padding-64 w3-container">
+        <div class="w3-content">
+          <div class="w3-twothird">
+            <p class="w3-text-grey">Choisir un domaine pour continuer.</p>
+          </div>
+
+          <div class="w3-third w3-center">
+            <i class="fa fa-anchor w3-padding-64 w3-text-red"></i>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
 
     let content;
@@ -35,31 +62,63 @@ class App extends React.Component {
         <DomaineElement />
       );
     } else {
-      content = (
-        <p>Choisir un domaine</p>
-      );
+      content = this.contenuPageAccueil();
     }
 
-    let nav = (
-      <nav>
-        <div>
-          <div>
-            <button className="aslink" onClick={this.afficherAccueil} value="">Accueil</button>
-            <button className="aslink" onClick={this.changerDomaine} value="SenseursPassifs">Senseurs Passifs</button>
-          </div>
-        </div>
-      </nav>
-    )
-
+    let header = this.header();
     return (
         <div className="App">
-          <h1>Vitrine sur la MilleGrille XXXX</h1>
-          {nav}
+          <ToggleMenu />
+          {header}
           {content}
         </div>
     );
   }
 
+}
+
+class ToggleMenu extends PureComponent {
+
+  state = {
+    show: false,
+  }
+
+  toggleMenu = event => {
+    this.setState({show: !this.state.show});
+  }
+
+  render() {
+    let mobileMenu;
+    if(this.state.show) {
+      mobileMenu = (
+        <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large w3-show">
+          <button className="w3-bar-item w3-button w3-padding-large" onClick={this.afficherAccueil} value="">Accueil</button>
+          <button className="w3-bar-item w3-button w3-padding-large" onClick={this.changerDomaine} value="SenseursPassifs">Senseurs Passifs</button>
+        </div>
+      );
+    } else {
+      mobileMenu = null;
+    }
+
+    let content = (
+      <nav class="w3-top">
+        <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+          <button
+            class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red"
+            onClick={this.toggleMenu}
+            title="Toggle Navigation Menu">
+              <i class="fa fa-bars"></i>
+          </button>
+          <button className="w3-bar-item w3-button w3-padding-large w3-white" onClick={this.afficherAccueil} value="">Accueil</button>
+          <button className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" onClick={this.changerDomaine} value="SenseursPassifs">Senseurs Passifs</button>
+        </div>
+
+        {mobileMenu}
+      </nav>
+    );
+
+    return content;
+  }
 }
 
 export default App;
