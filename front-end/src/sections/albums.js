0a0bf49070b1f592f4898d46b52ea0807d1fd4f7
Mathieu Dugre <mathieu.dugre@mdugre.info>
2019-12-30 09:32:58 -0500

    Ajout card columns.

--- front-end/src/sections/albums.js
+++ front-end/src/sections/albums.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Card, CardColumns} from 'react-bootstrap';
 import {SectionVitrine} from './sections';
 
 import './albums.css';
@@ -17,7 +18,90 @@ export class AlbumsVitrine extends SectionVitrine {
 
   render() {
     return (
-      <div>Allo album<br/>Allo album<br/>Allo album<br/>Allo album<br/></div>
+      <div>
+        {this._renderRecent()}
+      </div>
     );
+  }
+
+  _renderRecent() {
+    return (
+      <CardColumns>
+        <Card>
+          <Card.Img variant="top" src="res/guido-hofmann-G7BDFhrV00E-unsplash.jpg" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="res/guido-hofmann-G7BDFhrV00E-unsplash.jpg" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="res/guido-hofmann-G7BDFhrV00E-unsplash.jpg" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="res/guido-hofmann-G7BDFhrV00E-unsplash.jpg" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="res/guido-hofmann-G7BDFhrV00E-unsplash.jpg" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="res/guido-hofmann-G7BDFhrV00E-unsplash.jpg" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="res/guido-hofmann-G7BDFhrV00E-unsplash.jpg" />
+          <Card.Body>
+            <Card.Title>Card title that wraps to a new line</Card.Title>
+            <Card.Text>
+              This is a longer card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardColumns>
+    )
+  }
+
+  _renderCollections() {
+
   }
 }
