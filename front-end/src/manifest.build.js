a5c0fe51fb8c14464c68b574dd8c014fcba06b10
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-01-06 03:10:56 +0000

    Fix configuration consignation et data.
    Vitrine avec nginx fonctionne en deploiement local.

--- front-end/src/manifest.build.js
+++ front-end/src/manifest.build.js
@@ -1,5 +1,5 @@
 const build = {
-  date: '2020-01-06 01:50',
+  date: '2020-01-06 02:50',
   version: '1.18.2'
 }
 module.exports = build;
--- static/docker-run.sh
+++ static/docker-run.sh
@@ -5,7 +5,7 @@ IMAGENAME="$NAME:${ARCH}_${VERSION}"
 
 docker run --rm -it \
       --mount "type=bind,source=/home/mathieu/mgdev/certs,target=/certs" \
-      --mount "type=bind,source=/var/opt/millegrilles,target=/usr/share/nginx/millegrilles" \
-      --env LISTE_MILLEGRILLES='IDMG=33KRMhqcWCKvMHyY5xymMCUEbT53Kg1NqUb9AU6:WEB_URL=www.millegrilles.com:WEB_CERT=/certs/33KRMhqcWCKv.pki.nginx.fullchain.20191219a:WEB_KEY=/certs/33KRMhqcWCKv.pki.nginx.key.20191219a' \
+      --mount "type=bind,source=/home/mathieu/mgdev/millegrilles,target=/usr/share/nginx/millegrilles" \
+      --env LISTE_MILLEGRILLES='IDMG=33KRMhqcWCKvMHyY5xymMCUEbT53Kg1NqUb9AU6:WEB_URL=mg-dev3.maple.maceroc.com:WEB_CERT=/certs/33KRMhqcWCKv.pki.nginx.fullchain.20191219a:WEB_KEY=/certs/33KRMhqcWCKv.pki.nginx.key.20191219a' \
       -p 6443:443 \
       $IMAGENAME bash
