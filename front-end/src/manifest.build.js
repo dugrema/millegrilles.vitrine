a0bb6fe28e4c9798b102868a349aae34b5fe328e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-01-25 16:41:37 +0000

    Changement nom docker
    vitrine_api vers vitrineapi (id pour vitrineweb)

--- front-end/src/manifest.build.js
+++ front-end/src/manifest.build.js
@@ -1,5 +1,5 @@
 const build = {
-  date: '2020-01-25 14:28',
-  version: '1.18.7'
+  date: '2020-01-25 16:41',
+  version: '1.18.8'
 }
 module.exports = build;
--- front-end/src/manifest.build.js.cp
+++ front-end/src/manifest.build.js.cp
@@ -1,5 +1,5 @@
 const build = {
-  date: '2020-01-25 14:25',
+  date: '2020-01-25 14:28',
   version: '1.18.7'
 }
 module.exports = build;
