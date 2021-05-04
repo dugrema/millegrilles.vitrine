34d860a883451662496fb97fb142779a52e0235e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-05-04 15:16:57 +0000

    Revert translation avec import
    Simplifie chargement ressources pour distribution CDN

--- client/src/components/i18n.js
+++ client/src/components/i18n.js
@@ -1,24 +1,24 @@
-import i18n from "i18next";
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from "react-i18next";
+import i18n from "i18next"
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from "react-i18next"
 
-import moment from 'moment';
+import moment from 'moment'
 
 // the translations
 import 'moment/locale/fr'
-// import fr from './vitrine.fr.json';
-// import en from './vitrine.en.json';
-// const resources = {fr, en};
+import fr from './translation.fr.json'
+import en from './translation.en.json'
+const resources = {fr, en}
 
 function formatterValeur(value, format, lng) {
   if(value instanceof Date) {
-    return moment(value).locale(lng).format(format);
+    return moment(value).locale(lng).format(format)
   } else if(!isNaN(value) && !isNaN(format)) {
-    return Number(value).toFixed(format);
+    return Number(value).toFixed(format)
   }
 
-  return value;
+  return value
 }
 
 i18n
@@ -26,21 +26,21 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    // resources,
+    resources,
     fallbackLng: 'fr',
 
     // debug: true,
 
     keySeparator: '.', // we use keys in form messages.welcome
 
-    backend: {
-      loadPath: './locales/{{lng}}/{{ns}}.json',
-    },
+    // backend: {
+    //   loadPath: './locales/{{lng}}/{{ns}}.json',
+    // },
 
     interpolation: {
       escapeValue: false, // react is already safe from xss,
       format: formatterValeur
     }
-  });
+  })
 
-export default i18n;
+export default i18n
