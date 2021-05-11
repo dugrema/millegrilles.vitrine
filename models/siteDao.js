a51a4b7a0584dd6dcab1bc68ed5f1e89d7ca33c8
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-05-11 15:22:02 +0000

    Cleanup code obsolete

--- models/siteDao.js
+++ models/siteDao.js
@@ -3,7 +3,6 @@ const {sauvegarderSites, sauvegarderPosts, sauvegarderCollections} = require('..
 const {extrairePostids, extraireCollectionsRecursif} = require('../models/siteModel')
 
 async function chargerSites(amqpdao, noeudId) {
-
   const domaineAction = 'Publication.configurationSitesNoeud',
         requete = {noeud_id: noeudId}
 
@@ -12,35 +11,6 @@ async function chargerSites(amqpdao, noeudId) {
   // const messageSites = await _chargerSites(amqpdao, noeudId)
   debug("siteDao.chargerSites message configuration : %O", messageSites)
   await sauvegarderSites(noeudId, messageSites, amqpdao)
-
-  // Extraire post ids
-  const postIdMap = {}
-  var toutesCollections = false,
-      collectionIds = {}
-
-
-  // const collectionsInfo = extraireCollectionsRecursif(site)
-  // debug("Collections du site %s : %O", site.site_id, collectionsInfo)
-  // collectionsInfo.collections.forEach(id=>{collectionIds[id]=true})  // Conserver Ids, dedupe
-
-  // const messagePosts = await chargerPosts(amqpdao, Object.keys(postIdMap))
-  // debug("Posts recus : %O", messagePosts)
-  // await sauvegarderPosts(messagePosts, amqpdao)
-  //
-  // var messageCollections = null
-  // if(toutesCollections) {
-  //   debug("Charger toutes les collections publiques")
-  //   messageCollections = await chargerCollection(amqpdao)
-  // } else {
-  //   collections = Object.keys(collectionIds)
-  //   debug("Charger collections publiques : %O", collections)
-  //   messageCollections = await chargerCollection(amqpdao, collections)
-  // }
-  //
-  // if(messageCollections) {
-  //   await sauvegarderCollections(messageCollections, amqpdao)
-  // }
-
 }
 
 async function chargerSections(amqpdao, noeudId) {
@@ -52,27 +22,5 @@ async function chargerSections(amqpdao, noeudId) {
   const confirmation = await amqpdao.transmettreCommande(domaineAction, commande, {decoder: true})
   debug("Confirmation pousser sections : %O", confirmation)
 }
-
-// async function chargerPosts(amqpdao, postIds) {
-//   debug("Charger post ids: %O", postIds)
-//
-//   const domaineAction = 'Publication.posts',
-//         requete = {post_ids: postIds}
-//
-//   return await amqpdao.transmettreRequete(domaineAction, requete, {decoder: true})
-// }
-//
-// async function chargerCollection(amqpdao, collectionsIds) {
-//   debug("Charger collections ids : %O", collectionsIds)
-//
-//   const domaineAction = 'GrosFichiers.detailCollectionsPubliques',
-//         requete = {}
-//
-//   if(collectionsIds) {
-//     requete.collections = collectionsIds
-//   }
-//
-//   return await amqpdao.transmettreRequete(domaineAction, requete, {decoder: true})
-// }
 
 module.exports = {chargerSites, chargerSections}
